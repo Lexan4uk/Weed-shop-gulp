3175b4d2898e2e01f8a97e3a246bc566b580eb41
Alexey <alkatras46rus@gmail.com>
2024-06-04 16:00:07 +0300

    comm

---
+++ src/scripts/script.js
@@ -0,0 +1,7 @@
+document.addEventListener("DOMContentLoaded", function(){
+    // vendor
+    @@include('vendor/viewport-extra.min.js')
+    
+    // modules
+    @@include('module/supportWebp.js')
+})
