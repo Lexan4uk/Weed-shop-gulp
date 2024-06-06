f14305f70ff48255b3d341b74aad92196c183b35
Alexey <alkatras46rus@gmail.com>
2024-06-06 16:11:50 +0300

    comm

---
+++ src/scripts/module/sliders.js
@@ -0,0 +1,6 @@
+window.addEventListener('DOMContentLoaded', () => {
+    const presentation_slider = new Swiper('.presentation-block__presentation-slider', {
+        slidesPerView: 3,
+        spaceBetween: 32,
+    });
+})
--- src/scripts/script.js
+++ src/scripts/script.js
@@ -4,4 +4,5 @@ document.addEventListener("DOMContentLoaded", function(){
     
     // modules
     @@include('module/supportWebp.js')
+    @@include('module/sliders.js')
 })
