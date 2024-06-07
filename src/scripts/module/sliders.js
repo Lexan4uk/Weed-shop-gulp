dd41ca8012a0243f931d8a800e88bf020c31b85e
Alexey <alkatras46rus@gmail.com>
2024-06-07 15:29:11 +0300

    comm

--- src/scripts/module/sliders.js
+++ src/scripts/module/sliders.js
@@ -2,5 +2,38 @@ window.addEventListener('DOMContentLoaded', () => {
     const presentation_slider = new Swiper('.presentation-block__presentation-slider', {
         slidesPerView: 3,
         spaceBetween: 32,
+        breakpoints : {
+            0: {
+                slidesPerView: 1,
+            },
+            500: {
+                slidesPerView: 2,
+            },
+            769: {
+                slidesPerView: 3,
+            }
+        },
+        navigation: {
+            prevEl: '.presentation-swiper-button-prev',
+            nextEl: '.presentation-swiper-button-next'
+          }
+    });
+    const feedback_slider = new Swiper('.feedback-block__feedback-slider', {
+        slidesPerView: 2,
+        spaceBetween: 32,
+        breakpoints : {
+            0: {
+                slidesPerView: 1,
+            },
+            769: {
+                slidesPerView: 2,
+            }
+            
+        },
+        navigation: {
+            prevEl: '.feedback-swiper-button-prev',
+            nextEl: '.feedback-swiper-button-next'
+          }
+        
     });
 })
